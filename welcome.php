8e5a25844c9f8a38aa7bd20193c17e8cdf947d57
Alok232101 <alokg5251@gmail.com>
2023-12-29 16:18:02 +0530

    add project to git

---
+++ welcome.php
@@ -0,0 +1,62 @@
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
+    <title> Furniture </title>
+    <link rel="stylesheet" type="text/css" href="main.css" />
+    <style type="text/css">
+        body {
+            width: 100%;
+            background: url(Wwww.jpg);
+            background-position: center center;
+            background-repeat: no-repeat;
+            background-attachment: fixed;
+            background-size: cover;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="banner" >
+        <div class="navbar" ">
+
+
+
+            <ul >
+               &nbsp; &nbsp;&nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp;&nbsp;
+                &nbsp; &nbsp;&nbsp; &nbsp; &nbsp;&nbsp;
+                &nbsp; &nbsp;&nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp;&nbsp;
+                &nbsp; &nbsp;&nbsp; &nbsp; &nbsp;&nbsp;
+                &nbsp; &nbsp;&nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp;&nbsp;
+                &nbsp; &nbsp;&nbsp; &nbsp; &nbsp;&nbsp;
+                <li ><a href="index.php">HOME</a></li> &nbsp;
+                &nbsp; &nbsp;&nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp;&nbsp;
+                &nbsp; &nbsp;&nbsp; &nbsp; &nbsp;&nbsp;
+                <li><a href="admin/index.php">ADMIN</a></li>
+                &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp;&nbsp; &nbsp;
+                &nbsp;&nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp;&nbsp;
+             
+
+            </ul>
+        </div>
+
+        <div class="content">
+            <P>If you have knowledge,<br>let others light their candles in it.
+            </P>
+            <div>
+                <button type="button"><span></span>LINKEDIN</button>
+                <button type="button"><span></span>GITHUB</button>
+                <button type="button"><span></span>LEARN MORE</button>
+
+            </div>
+
+
+        </div>
+
+    </div>
+
+</body>
+
+</html>
